40a270815e122569ed90f54ad8d7344cc19d995a
Harrison <harrison@theshoebridges.com>
2015-02-14 19:23:53 +1100

    bot: add basic searching and stream finding and add config, example_config: basics

--- bot.py
+++ bot.py
@@ -1,18 +1,35 @@
 from praw import *
-from urllib import request
+
+import random
 import os
 import requests
+import json
 
-BOT_NAME = os.environ.get('BOT_NAME')
-BOT_PASSWORD = os.environ.get('BOT_PASSWORD')
-SUBREDDIT = os.getenv('SUBREDDIT', 'WatchPeopleCode')
+config_data = open('config.json')
+config = json.load(config_data)
+
+BOT_NAME = config["bot"]["username"]
+BOT_PASSWORD = config["bot"]["password"]
+SUBREDDIT = config["subreddit"] or "watchpeoplecode"
 
 r = Reddit('Get the top 3 streams from r/WatchPeopleCode')
 r.login(BOT_NAME, BOT_PASSWORD)
 
 data = requests.get("http://www.watchpeoplecode.com/json").json()
 
-print(data)
+live_streams = data["stream_urls"]
+top_streams = []
+if len(live_streams) >= 3:
+	chosen = []
+	for z in range(0, 3):
+		i = 0
+		while i not in chosen:
+			i = random.randint(0, len(live_streams))
+		
+		top_streams.append(live_streams[i])
+		chosen.append(i)
+else:
+	top_streams = live_streams
 
-r.close()
+config_data.close()
 
