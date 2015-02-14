6bc1d8be24ba2ab78ff751fada7cf7717b2a8bf6
Harrison <harrison@theshoebridges.com>
2015-02-14 19:32:38 +1100

    bot: add basic documentation and a template for outputs

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import os
 import requests
 import json
 
+# Open up the bots config.
 config_data = open('config.json')
 config = json.load(config_data)
 
+# Set credentials for the bot.
 BOT_NAME = config["bot"]["username"]
 BOT_PASSWORD = config["bot"]["password"]
 SUBREDDIT = config["subreddit"] or "watchpeoplecode"
@@ -15,8 +17,10 @@ SUBREDDIT = config["subreddit"] or "watchpeoplecode"
 r = Reddit('Get the top 3 streams from r/WatchPeopleCode')
 r.login(BOT_NAME, BOT_PASSWORD)
 
+# Get streams and convert them to JSON.
 data = requests.get("http://www.watchpeoplecode.com/json").json()
 
+# Choose 3 random streams to display in sidebar.
 live_streams = data["stream_urls"]
 top_streams = []
 if len(live_streams) >= 3:
@@ -30,6 +34,14 @@ if len(live_streams) >= 3:
 		chosen.append(i)
 else:
 	top_streams = live_streams
+
+output = ""
+for stream in top_streams:
+	output += "Check out [{}]({}) streaming".format(stream["user"], stream["url"])
+
+print(output)
+
+# TODO: Set sidebar to output.
 
 config_data.close()
 
