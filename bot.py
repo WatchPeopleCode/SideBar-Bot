350e2d80dfda46abb55279eac7470d25d737479e
Harrison <harrison@theshoebridges.com>
2015-02-15 10:01:35 +1100

    bot: rewrote using classes to make everything neater!

--- bot.py
+++ bot.py
@@ -15,43 +15,75 @@ import json
 config_data = open('config.json')
 config = json.load(config_data)
 
-# Set credentials for the bot.
-BOT_NAME = config["bot"]["username"]
-BOT_PASSWORD = config["bot"]["password"]
-SUBREDDIT = config.get("subreddit") or "watchpeoplecode"
-MODE = config.get("mode") or "live"
-DESCRIPTION = config["description"]
+class Bot:
+	def __init__(self, username, password, subreddit="watchpeoplecode", debug=False):
+		self._username = username
+		self._password = password
+		self._subreddit = subreddit
+		self.debug = debug
 
-r = Reddit('Get the top 3 streams from r/WatchPeopleCode')
-r.login(BOT_NAME, BOT_PASSWORD)
+		self.r = Reddit("{0} a bot for /r/{1} to set top WPC streams!".format(self._username, self._password))
 
-# Get streams and convert them to JSON.
-data = requests.get("http://www.watchpeoplecode.com/json").json()
+		self.login()
 
-# Choose 3 random streams to display in sidebar.
-live_streams = data[MODE]
-top_streams = []
-if len(live_streams) > 3:
-	chosen = []
-	for z in range(0, 3):
-		i = 0
-		while i in chosen:
-			i = random.randint(0, len(live_streams))
-		
-		top_streams.append(live_streams[i])
-		chosen.append(i)
-else:
-	top_streams = live_streams
+	def login(self):
+		self.log("I'm logging in now!")
+		self.r.login(self._username, self._password)
 
-# Assemble the description.
-output = ""
-for stream in top_streams:
-	output += "You should check out [{0}](http://reddit.com/r/{0}) streaming [{1}]({2})\n\n".format(stream["username"],
-														   stream["title"],
-														   stream["url"])
-subreddit = r.get_subreddit(SUBREDDIT)
-subreddit.update_settings(description="{}\n====\n{}".format(DESCRIPTION, output).encode('utf8'))
-print(output)
+	def log(self, message):
+		if not self.debug:
+			return
+		print("[{}] {}".format(self._username, message))
 
+
+class SidebarBot(Bot):
+	def __init__(self, username, password, description, subreddit="watchpeoplecode", mode="live", debug=False):
+		super().__init__(username, password, subreddit, debug)
+
+		self.description = description
+		self.mode = mode
+
+	def update(self):
+		self.log("Just about to start making that sweet description of yours!")
+		description = self.generate_description(self.choose_streams())
+		subreddit = self.r.get_subreddit(self._subreddit)
+		subreddit.update_settings(description=description.encode('utf8'))
+		self.log("Whew! Just updated your subreddits sidebar!")
+
+	def generate_description(self, streams):
+		output = self.description + "\n\n"
+		for stream in streams:
+			output += "You should check out [{0}](http://reddit.com/r/{0}) streaming [{1}]({2})\n\n".format(stream["username"], stream["title"], stream["url"])
+
+		return output
+
+	def choose_streams(self):
+		streams = self._get_streams()
+		live_streams = streams[self.mode]
+		top_streams = []
+		if len(live_streams) > 3:
+			chosen = []
+			for z in range(0, 3):
+				i = 0
+				while i in chosen:
+					i = random.randint(0, len(live_streams))
+				
+				top_streams.append(live_streams[i])
+				chosen.append(i)
+		else:
+			top_streams = live_streams
+
+		return top_streams
+
+	def _get_streams(self):
+		return requests.get("http://www.watchpeoplecode.com/json").json()
+
+sb = SidebarBot(config["bot"]["username"],
+				config["bot"]["password"],
+				config["description"],
+				subreddit=config["subreddit"],
+				mode=config["mode"],
+				debug=True)
+
+sb.update()
 config_data.close()
-
