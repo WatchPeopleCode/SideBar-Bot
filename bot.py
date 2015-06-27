5cedc7f3ad46978f660e84e373a09bad776715e9
Alexander Putilin <avp-13@yandex.ru>
2015-06-27 16:59:10 +0400

    "username" -> "user" in json + use python logging instead of homemade print logging

--- bot.py
+++ bot.py
@@ -13,58 +13,55 @@ import os
 import requests
 import json
 import time
+import logging
 from urllib.parse import urlparse, parse_qs
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(process)d - %(name)s - %(levelname)s - %(message)s')
 
 class Bot:
 	"""
 	Bot represents a basic bot, with a reddit username, password and subreddit
 	"""
-	def __init__(self, username, password, subreddit="watchpeoplecode", debug=False):
+	def __init__(self, username, password, subreddit="watchpeoplecode"):
 		self._username = username
 		self._password = password
 		self._subreddit = subreddit
-		self.debug = debug
 
 		self.r = Reddit("{0} a bot for /r/{1} to set top WPC streams!".format(self._username, self._subreddit))
 
 		self.login()
 
 	def login(self):
-		self.log("I'm logging in now!")
+		logging.info("I'm logging in now!")
 		self.r.login(self._username, self._password)
-
-	def log(self, message):
-		if not self.debug:
-			return
-		print("[{}] {}".format(self._username, message))
 
 
 class SidebarBot(Bot):
 	"""
 	Sidebar bot, a bot to update your sidebar!
 	"""
-	def __init__(self, username, password, description, subreddit="watchpeoplecode", mode="live", debug=False):
-		super().__init__(username, password, subreddit, debug)
+	def __init__(self, username, password, description, subreddit="watchpeoplecode", mode="live"):
+		super().__init__(username, password, subreddit)
 		if mode != 'live':
 			raise Exception("Only 'live' mode is supported")
 
 		self.description = description
 
 	def update(self):
-		self.log("Just about to start making that sweet description of yours!")
+		logging.info("Just about to start making that sweet description of yours!")
 		description = self.generate_description(self.choose_streams())
 
-		self.log("Whew! Just gonna get that specified subreddit for you!")
+		logging.info("Whew! Just gonna get that specified subreddit for you!")
 		subreddit = self.r.get_subreddit(self._subreddit)
-		self.log("Updating your sidebar now...")
+		logging.info("Updating your sidebar now...")
 		subreddit.update_settings(description=description.encode('utf8'))
-		self.log("Updated your sidebar")
+		logging.info("Updated your sidebar")
 
 	def generate_description(self, streams):
 		output = self.description["pre"] + "\n\n"
 		output += self.description["viewers_template"].format(str(self._get_total_viewers())) + "\n\n"
 		for stream in streams:
-			output += self.description["template"].format(stream["username"], stream["title"], stream["url"], self._get_viewers(stream)) + "\n\n"
+			output += self.description["template"].format(stream["user"], stream["title"], stream["url"], self._get_viewers(stream)) + "\n\n"
 
 		output += "\n\n" + self.description["post"]
 		return output
@@ -101,7 +98,6 @@ if __name__ == '__main__':
 		subreddit = config["subreddit"]
 		timer = int(config["timer"])
 		mode = config["mode"]
-		debug = config["debug"]
 
 		config_data.close()
 	else:
@@ -113,22 +109,20 @@ if __name__ == '__main__':
 					   "template": os.environ['DESCRIPTION_TEMPLATE'],
 					   "post": os.environ['DESCRIPTION_POST']}
 		subreddit = os.environ['SUBREDDIT']
-		debug = os.environ['DEBUG']
 		timer = int(os.environ['TIMER'])
 
 	sb = SidebarBot(username,
 					password,
 					description,
 					subreddit=subreddit,
-					mode=mode,
-					debug=debug)
+					mode=mode)
 	while True:
 		try:
 			sb.update()
 		except errors.InvalidCaptcha:
-			print("WARNING: No Captcha Supplied\nIt worked anyway though...\nSuppress this message by getting 2 link karma.")
+			logging.warning("No Captcha Supplied\nIt worked anyway though...\nSuppress this message by getting 2 link karma.")
 		except Exception as e:
-			print("FAILED: todo mail harrison and aaron")
-			print(e)
+			logging.error("todo mail harrison and aaron")
+			logging.exception(e)
 			# mail harrison, and aaron.
 		time.sleep(timer)
